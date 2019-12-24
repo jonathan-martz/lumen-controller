e4e6d6712763c8ff33c808c5d67b25f51ba412db
jonathan-martz <github@jmartz.de>
2019-12-24 14:02:18 +0100

    add logic for xxs protection

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,81 +1,117 @@
 <?php
 
-	namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-	use \Illuminate\Http\Request;
-	use \Laravel\Lumen\Routing\Controller as BaseController;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ */
+class Controller extends BaseController
+{
+    /**
+     * @var array
+     */
+    public $result = [];
 
     /**
-     * Class Controller
-     * @package App\Http\Controllers
+     * @var Request
      */
-    class Controller extends BaseController
-	{
-        /**
-         * @var array
-         */
-        public $result =  [];
+    public $request;
+    /**
+     * @var array
+     */
+    public $message = [];
 
-        /**
-         * @var Request
-         */
-        public $request;
-        /**
-         * @var array
-         */
-        public $message = [];
+    /**
+     * @var array
+     */
+    public $xxsProtection = ['all' => true];
 
-        /**
-         * Controller constructor.
-         * @param Request $request
-         */
-        public function __construct(Request $request){
-		    $this->request = $request;
-			$this->addRequest('timestamp',\time());
-		}
 
-        /**
-         * @param string $key
-         * @param $value
-         */
-        public function addResult(string $key, $value):void{
-			$this->result[$key] = $value;
-		}
+    /**
+     * Controller constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->addRequest('timestamp', \time());
+    }
 
-        /**
-         * @param string $key
-         * @param $value
-         */
-        public function addRequest(string $key, $value):void{
-			$this->result[$key] = $value;
-		}
+    /**
+     * @throws Exception
+     */
+    public function performXxsProtection()
+    {
+        if (!empty($this->xxsProtection['all']) && $this->xxsProtection['all'] === true) {
+            foreach ($this->request->all() as $key => $input) {
+                if (strlen($input) !== strlen(strip_tags($input))) {
+                    throw new Exception('Request contains xss attack');
+                }
+            }
+        }
+    }
 
-        /**
-         * @param string $status
-         * @param string $message
-         */
-        public function addMessage(string $status, string $message):void{
-			$this->message[] = [
-				'status' => $status,
-				'message' => $message
-			];
-		}
+    /**
+     * @param string $key
+     * @param $value
+     */
+    public function addResult(string $key, $value): void
+    {
+        $this->result[$key] = $value;
+    }
 
-        /**
-         *
-         */
-        public function resetMessages():void{
-			$this->message[] = [];
-		}
+    /**
+     * @param string $key
+     * @param $value
+     */
+    public function addRequest(string $key, $value): void
+    {
+        $this->result[$key] = $value;
+    }
 
-        /**
-         * @return mixed
-         */
-        public function getResponse(){
-			return response()->json([
-				'result' => $this->result,
-				'request' => $this->request,
-				'message' => $this->message
-			]);
-		}
-	}
+    /**
+     * @param string $status
+     * @param string $message
+     */
+    public function addMessage(string $status, string $message): void
+    {
+        $this->message[] = [
+            'status' => $status,
+            'message' => $message
+        ];
+    }
+
+    /**
+     *
+     */
+    public function resetMessages(): void
+    {
+        $this->message = [];
+    }
+
+
+    /**
+     * @return JsonResponse
+     */
+    public function getResponse(): JsonResponse
+    {
+        try{
+            $this->performXxsProtection();
+        }
+        catch(Exception $e){
+            $this->resetMessages();
+            $this->addMessage('exception', $e->getMessage());
+        }
+        return response()->json([
+            'result' => $this->result,
+            'request' => $this->request,
+            'message' => $this->message
+        ]);
+    }
+}

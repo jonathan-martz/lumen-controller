a94b5aa02158b3c7be7e6602cb52b10b039e9b45
jonathan-martz <github@jmartz.de>
2019-12-24 12:49:30 +0100

    add request via constructor

--- controller/Controller.php
+++ controller/Controller.php
@@ -5,36 +5,73 @@
 	use \Illuminate\Http\Request;
 	use \Laravel\Lumen\Routing\Controller as BaseController;
 
-	class Controller extends BaseController
+    /**
+     * Class Controller
+     * @package App\Http\Controllers
+     */
+    class Controller extends BaseController
 	{
-		public $result =  [];
-		public $request = [];
-		public $message = [];
+        /**
+         * @var array
+         */
+        public $result =  [];
 
-		public function __construct(){
+        /**
+         * @var Request
+         */
+        public $request;
+        /**
+         * @var array
+         */
+        public $message = [];
+
+        /**
+         * Controller constructor.
+         * @param Request $request
+         */
+        public function __construct(Request $request){
+		    $this->request = $request;
 			$this->addRequest('timestamp',\time());
 		}
 
-		public function addResult(string $key, $value):void{
+        /**
+         * @param string $key
+         * @param $value
+         */
+        public function addResult(string $key, $value):void{
 			$this->result[$key] = $value;
 		}
 
-		public function addRequest(string $key, $value):void{
+        /**
+         * @param string $key
+         * @param $value
+         */
+        public function addRequest(string $key, $value):void{
 			$this->result[$key] = $value;
 		}
 
-		public function addMessage(string $status, string $message):void{
+        /**
+         * @param string $status
+         * @param string $message
+         */
+        public function addMessage(string $status, string $message):void{
 			$this->message[] = [
 				'status' => $status,
 				'message' => $message
 			];
 		}
 
-		public function resetMessages():void{
+        /**
+         *
+         */
+        public function resetMessages():void{
 			$this->message[] = [];
 		}
 
-		public function getResponse(){
+        /**
+         * @return mixed
+         */
+        public function getResponse(){
 			return response()->json([
 				'result' => $this->result,
 				'request' => $this->request,

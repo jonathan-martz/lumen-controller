6071c52e9a4d7f8e8d2b1e006585eb955a088edc
jonathan-martz <github@jmartz.de>
2020-01-03 22:06:42 +0100

    fix import App

--- controller/Controller.php
+++ controller/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use function time;
 

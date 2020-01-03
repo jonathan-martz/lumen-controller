23f742f3b6951a5010ad216ef61055e65dec6de8
jonathan-martz <github@jmartz.de>
2020-01-03 22:16:18 +0100

    fix class name Provider

--- provider/ControllerProvider.php
+++ provider/ControllerProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class ControllerRrovider extends ServiceProvider
+class ControllerProvider extends ServiceProvider
 {
     /**
      * Register any application services.
